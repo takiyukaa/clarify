7096365fd5e929b98e628f2f84e8ad64b3077ab6
Hiroki Kameyama <kamehiro1411@gmail.com>
2020-03-03 14:45:40 +0900

    created 4 tables, products, ingredients, product_ingredients and flags

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_024915) do
+ActiveRecord::Schema.define(version: 2020_03_03_043456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flags", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_flags_on_ingredient_id"
+    t.index ["user_id"], name: "index_flags_on_user_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.text "effect"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "brand"
+    t.text "description"
+    t.integer "barcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products_ingredients", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_products_ingredients_on_ingredient_id"
+    t.index ["product_id"], name: "index_products_ingredients_on_product_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +62,8 @@ ActiveRecord::Schema.define(version: 2020_03_03_024915) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "flags", "ingredients"
+  add_foreign_key "flags", "users"
+  add_foreign_key "products_ingredients", "ingredients"
+  add_foreign_key "products_ingredients", "products"
 end
