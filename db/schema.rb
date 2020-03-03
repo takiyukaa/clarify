08fc6c4fb72928f31b76e6ddae2e902718bfbb61
Hiroki Kameyama <kamehiro1411@gmail.com>
2020-03-03 17:56:55 +0900

    Created products controller and index and show views

--- db/schema.rb
+++ db/schema.rb
@@ -92,10 +92,6 @@ ActiveRecord::Schema.define(version: 2020_03_03_063360) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name"
-    t.string "second_name"
-    t.string "gender"
-    t.string "city"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
