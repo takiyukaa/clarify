8cc8590bad64c964443c6736942fc621fd4d2f0f
Hiroki Kameyama <kamehiro1411@gmail.com>
2020-03-03 18:56:34 +0900

    add data on product show view

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,8 @@
 class Ingredient < ApplicationRecord
   # acts_as_taggable_on :tags
   has_many :flags
-  has_many :products_ingredient
-  has_many :products, through: :products_ingredient
+  has_many :products_ingredients
+  has_many :products, through: :products_ingredients
   has_many :users, through: :flag
 
   validates :name, presence: true
