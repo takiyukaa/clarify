c3bca1a8dba4c3304b64a7d2766ce205b8aa4bcd
Hiroki Kameyama <kamehiro1411@gmail.com>
2020-03-04 16:43:25 +0900

    implemented scandit and changed datatype of barcode to bigint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_071955) do
+ActiveRecord::Schema.define(version: 2020_03_04_071958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_071955) do
     t.string "category"
     t.string "brand"
     t.text "description"
-    t.integer "barcode"
+    t.bigint "barcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -113,6 +113,10 @@ ActiveRecord::Schema.define(version: 2020_03_03_071955) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "second_name"
+    t.string "gender"
+    t.string "city"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
