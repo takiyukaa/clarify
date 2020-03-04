26864248ba461981f97fb33032de3b696a1bd448
Frances Cheng <francesvcheng@gmail.com>
2020-03-04 11:43:22 +0900

    seeds updated

--- db/schema.rb
+++ db/schema.rb
@@ -113,6 +113,10 @@ ActiveRecord::Schema.define(version: 2020_03_03_071955) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "second_name"
+    t.string "gender"
+    t.string "city"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
